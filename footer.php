1b9311473dcb5a054054e2b1cee781e28402105f
malinky <craig@malinkymedia.com>
2015-03-10 17:05:13 +0000

    Update to navicon and mobile icons in nav. Add livereload

--- footer.php
+++ footer.php
@@ -46,6 +46,13 @@
 <?php } ?>
 		
 <?php wp_footer(); ?>
-	
+
+<?php if ( WP_ENV == 'local' ) { ?>
+
+<!-- Live Reload -->
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+<?php } ?>
+
 </body>
 </html>
