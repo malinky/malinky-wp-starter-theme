5bf8554fd13b98f28b34ce74583b71bef9a9f7e2
Craig Ramsay <craig@malinky.media>
2016-02-17 11:47:19 +0000

    New maps script

--- js/googlemap.js
+++ js/googlemap.js
@@ -5,49 +5,81 @@ function malinky_initialize()
 {
 
     //If not map-canvas element.
-    if (!document.getElementById('map-canvas')) return;
+    if ( ! document.getElementById( 'malinky-map-canvas' ) ) return;
 
     var mapOptions = {
-        center: { lat: parseFloat(google_map_settings.lat), lng: parseFloat(google_map_settings.long) },
-        zoom: parseFloat(google_map_settings.zoom),
+        center: {
+            lat: parseFloat( google_map_settings[0].lat ), 
+            lng: parseFloat( google_map_settings[0].long ) 
+        },
+        zoom: parseFloat( google_map_settings.zoom ),
         scrollwheel: false,
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var map = new google.maps.Map( document.getElementById( 'malinky-map-canvas' ), mapOptions );
 
-    /*
-     * Marker.
-     */ 
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-    });
+    var key;
+    var markers = [];
+    var infoWindows = [];
 
-    if (google_map_settings.show_address_label == true) {
-
-        //Reopen info window when marker is clicked.
-        google.maps.event.addDomListener(marker, "click", function() {
-            infowindow.open(map, marker);
-        })
+    for ( var key in google_map_settings ) {
 
         /*
-         * Info window.
+         * Only get numerical keys which will be those containing a location.
          */
-        var contentString = '<div style="width: 180px; padding-right: 10px; font-size: 13px;">' + google_map_settings.address_label + '</div>';
-        var infowindow = new google.maps.InfoWindow( {
-            content: contentString
-        });
-        infowindow.open(map, marker);
+        if ( ! isNaN( key ) ) {
+
+            /*
+             * Markers.
+             */ 
+             markers[key] = new google.maps.Marker({
+                position: new google.maps.LatLng( parseFloat( google_map_settings[key].lat ), parseFloat( google_map_settings[key].long ) ),
+                map: map,
+            });
+
+            /*
+             * Create info windows if the addresses are set.
+             */
+            if ( google_map_settings[key].address_label != '' ) {
+
+                infoWindows[key] = new google.maps.InfoWindow( {
+                    content:    '<div class="malinky-map-info-window">' + 
+                                google_map_settings[key].address_label + 
+                                '</div>'
+                });
+
+                google.maps.event.addListener( markers[key], 'click', function( closureKey ) {
+                    return function() {
+                        //Close all infowindows.
+                        for ( var x = 0; x < infoWindows.length; x++ ) {
+                            infoWindows[x].close();
+                        }
+                        //Open new infowindow.
+                        infoWindows[closureKey].open( map, markers[closureKey] );
+                    }
+                }(key));
+
+                /*
+                 * If address label is to be shown on first load.
+                 */
+                if ( google_map_settings[key].show_address_label ) {
+                    infoWindows[key].open( map, markers[key] );
+                }
+
+            }
+
+        }
 
     }
+
 
     /*
      * Center on resize.
      */
-    google.maps.event.addDomListener(window, "resize", function() {
+    google.maps.event.addDomListener( window, 'resize', function() {
         var center = map.getCenter();
-        google.maps.event.trigger(map, "resize");
-        map.setCenter(center); 
+        google.maps.event.trigger( map, 'resize' );
+        map.setCenter( center );
     });
     
 }
